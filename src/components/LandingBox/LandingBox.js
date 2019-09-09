5eaecdd084a66167e53706a1116492623e0dc9b9
Jason Forman <20210580+JRForman@users.noreply.github.com>
2019-09-09 00:16:42 -0700

    Changed Landingbox statefull.  Added image loading

--- src/components/LandingBox/LandingBox.js
+++ src/components/LandingBox/LandingBox.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './LandingBox.module.css';
 // import pageClass from '../../App.css'
-import background from '../../assets/coverImage.jpg'; 
+import background from '../../assets/coverImage.jpg';
 import NavItem from '../Navigation/NavBar/NavItem/NavItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -11,60 +11,77 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 
 library.add(fab, fas);
 
-const LandingBox = () => {
-	
-	return (
-			<div className={classes.LandingBox}>
-				<img src={background} alt={"me"} />
-				<h1>Hi. I'm Jason and I like to create solutions.</h1>
-				<div className={classes.linkContainer}>
-					<div className={classes.NavIcons}>
-						<a
-							className={classes.NavIcon}
-							href='https://www.linkedin.com/in/jasonrforman'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							<FontAwesomeIcon
-								icon={['fab', 'linkedin']}
-								size='lg'
-								className='NavIcon'
-							/>
-						</a>
-						<a
-							className={classes.NavIcon}
-							href='https://github.com/JRForman'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							<FontAwesomeIcon
-								icon={['fab', 'github']}
-								size='lg'
-								className='NavIcon'
-							/>
-						</a>
-						<a
-							className={classes.NavIcon}
-							href='mailto:contact@jrforman.com'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							<FontAwesomeIcon
-								icon={['fas', 'envelope']}
-								size='lg'
-								className='NavIcon'
-							/>
-						</a>
-					</div>
-					<NavItem link='/projects' exact>
-						Projects
-				</NavItem>
-					<NavItem link='/aboutme'>About Me</NavItem>
-					<NavItem link='/other'>Other Projects</NavItem>
-				</div>
-			</div>
+class LandingBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { imageIsReady: false };
+	}
+	componentDidMount() {
+		const img = new Image();
+		img.src = background;
+		img.onload = () => {
+			this.setState({ imageIsReady: true });
+		};
+	}
 
-	);
-};
+	render() {
+		const { imageIsReady } = this.state;
+		if (!imageIsReady) {
+			return <div>Loading...............</div>;
+		} else {
+			return (
+				<div className={classes.LandingBox}>
+					<h1>Hi. I'm Jason and I like to create solutions.</h1>
+					<img src={background} alt={'me'} />
+					<div className={classes.linkContainer}>
+						<div className={classes.NavIcons}>
+							<a
+								className={classes.NavIcon}
+								href='https://www.linkedin.com/in/jasonrforman'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<FontAwesomeIcon
+									icon={['fab', 'linkedin']}
+									size='lg'
+									className='NavIcon'
+								/>
+							</a>
+							<a
+								className={classes.NavIcon}
+								href='https://github.com/JRForman'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<FontAwesomeIcon
+									icon={['fab', 'github']}
+									size='lg'
+									className='NavIcon'
+								/>
+							</a>
+							<a
+								className={classes.NavIcon}
+								href='mailto:contact@jrforman.com'
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<FontAwesomeIcon
+									icon={['fas', 'envelope']}
+									size='lg'
+									className='NavIcon'
+								/>
+							</a>
+						</div>
+						<NavItem link='/projects' exact>
+							Projects
+						</NavItem>
+						<NavItem link='/aboutme'>About Me</NavItem>
+						<NavItem link='/other'>Other Projects</NavItem>
+					</div>
+				</div>
+			);
+		}
+	}
+}
 
 export default LandingBox;
