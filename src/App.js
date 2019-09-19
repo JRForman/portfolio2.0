d97958a15b9cee624ff81505234324797279dd56
Jason Forman <20210580+JRForman@users.noreply.github.com>
2019-09-19 06:25:03 -0700

    Un-named routes default to landing.

--- src/App.js
+++ src/App.js
@@ -7,43 +7,46 @@ import LandingBox from './components/LandingBox/LandingBox';
 import AboutMe from './components/AboutMe/AboutMe';
 import Headline from './components/Navigation/NavBar/Headline/Headline';
 import Solutions from './components/Solutions/Solutions';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
 			<div className={classes.App}>
-				<Route path='/' exact component={LandingBox} />
-				<Route
-					path='/Projects'
-					render={props => (
-						<>
-							<Headline />
-							<NavBar />
-							<Projects />
-						</>
-					)}
-				/>
-				<Route
-					path='/AboutMe'
-					render={props => (
-						<>
-							<Headline />
-							<NavBar />
-							<AboutMe />
-						</>
-					)}
-				/>
-				<Route
-					path='/Solutions'
-					render={props => (
-						<>
-							<Headline />
-							<NavBar />
-							<Solutions/>
-						</>
-					)}
-				/>
+				<Switch>
+					<Route path='/' exact component={LandingBox} />
+					<Route
+						path='/Projects'
+						render={props => (
+							<>
+								<Headline />
+								<NavBar />
+								<Projects />
+							</>
+						)}
+					/>
+					<Route
+						path='/AboutMe'
+						render={props => (
+							<>
+								<Headline />
+								<NavBar />
+								<AboutMe />
+							</>
+						)}
+					/>
+					<Route
+						path='/Solutions'
+						render={props => (
+							<>
+								<Headline />
+								<NavBar />
+								<Solutions />
+							</>
+						)}
+					/>
+					<Route component={LandingBox} />
+				</Switch>
 			</div>
 		);
 	}
